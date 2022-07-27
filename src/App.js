5f3c466be2c55d5612c0e00010ac5232c718a41b
Joel Parks <joel.parks@grapecity.com>
2022-07-27 09:25:11 -0400

    Wijmo Updates
    Updated application to include Wijmo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
+import * as wjCore from '@grapecity/wijmo';
+import * as wjcInput from '@grapecity/wijmo.react.input';
+
 function App() {
+  const [cv, setCollectionView] = useState(new wjCore.CollectionView([], {}));
+  useEffect(() => {
+    fetch('https://mocki.io/v1/c8e84317-feaa-40ea-89cf-2af1fb611d49').then(response => response.json()).then(data => cv.sourceCollection = data);
+  });
+  function customItemFormatter(index, content) {
+    let country = 'N/A';
+      for(var i = 0; i < cv.sourceCollection.length; i++) {
+        if(content === cv.sourceCollection[i].state) {
+          country = cv.sourceCollection[i].country;
+        }
+      }
+    content = '<span class="primary">' + content + ' </span><span class="secondary">' + country + '</span>';
+    return content;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <wjcInput.AutoComplete itemsSource={cv} displayMemberPath="state" headerPath="state" isContentHtml={true} itemFormatter={customItemFormatter.bind(this)}></wjcInput.AutoComplete>
     </div>
   );
 }
